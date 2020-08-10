abe99786d29b3c14b0fe0cdb1700e3fdc92d43ac
Joyce Brum <joycebrumu.u@gmail.com>
2020-08-10 19:43:24 -0300

    WIP: Creating DataBase class to deal with database
    Nullpointer exception to be fixed

---
+++ SQLGame/Assets/Scripts/TestingDataBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class TestingDataBase : MonoBehaviour
+{
+    public DataBase database;
+
+    public void Start()
+    {
+        string sql = "CREATE TABLE IF NOT EXISTS teste(name VARCHAR(20), score INT)";
+        database.NonQueryCommand(sql);
+        Debug.Log("Criado com sucesso");
+    }
+
+    public void Inserir()
+    {
+        string sql = "INSERT INTO teste(name, score) VALUES('Joyce', 7200)";
+        database.NonQueryCommand(sql);
+        Debug.Log("Inserido com sucesso");
+    }
+
+    public void Recuperar()
+    {
+        string sqlQuery = "SELECT * FROM teste";
+        IDataReader reader = database.QueryCommand(sqlQuery);
+
+        while (reader.Read())
+        {
+            string name = (string)reader["name"];
+            int value = (int)reader["score"];
+
+            Debug.Log("value = " + value + " name = " + name);
+        }
+    }
+}
