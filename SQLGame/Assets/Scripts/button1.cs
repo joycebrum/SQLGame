32cbeaffb6717dc3a606ee6f95bd30373ba260b3
Thiago Damasceno <topd97@yahoo.com.br>
2020-08-31 23:11:45 -0300

    Adicionando assets de botoes

---
+++ SQLGame/Assets/Scripts/button1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void onClick()
+    {
+        Debug.Log("oi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
