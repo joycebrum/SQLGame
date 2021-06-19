3bd5ac4579377130e666454f8ffcbc9cdb44ab6f
joycebrum <joycebrumu.u@gmail.com>
2021-06-19 18:29:46 -0300

    creating chat interface
    old interfae revomed and a new one created using scrollable list

--- SQLGame/Assets/Scripts/ChatBoxFunctions.cs
+++ SQLGame/Assets/Scripts/ChatBoxFunctions.cs
@@ -7,6 +7,18 @@ public class ChatBoxFunctions : MonoBehaviour
     [SerializeField] Transform messageParentPanel;
     [SerializeField] GameObject newMessagePrefab;
     string message = "";
+    //TODO: salvar o histórico para exibir
+    List<string> messages = null;
+
+    public void Start()
+    {
+        /*messages = new List<string> { "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?", "Olá tudo bem2?" };
+        foreach(string msg in messages)
+        {
+            this.message = msg;
+            this.ShowMessage();
+        }*/
+    }
 
     public void SetMessage(string message)
     {
@@ -18,8 +30,8 @@ public class ChatBoxFunctions : MonoBehaviour
         if (message != "") {
             GameObject clone = (GameObject)Instantiate(newMessagePrefab);
             clone.transform.SetParent(messageParentPanel);
-            print(messageParentPanel.childCount - 2);
-            clone.transform.SetSiblingIndex(messageParentPanel.childCount - 2);
+            //clone.transform.SetSiblingIndex(messageParentPanel.childCount - 2);
+            clone.transform.localScale = new Vector3(1f, 1f, 1f);
             clone.GetComponent<MessageFunctions>().ShowMessage(message);
         }
     }
