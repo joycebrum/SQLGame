c72f3639963aa9071586ad85dd678fe14cb820aa
Thiago <topd97@yahoo.com.br>
2021-07-18 19:05:03 -0300

    Creating table

--- SQLGame/Assets/Scripts/Screen Functions/DataBaseScreenFunctions.cs
+++ SQLGame/Assets/Scripts/Screen Functions/DataBaseScreenFunctions.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI.TableUI;
 
 public class DataBaseScreenFunctions: MonoBehaviour
 {
+    public TableUI table;
 
     List<string> headerMock = new List<string>();
     List<List<string>> tableMock = new List<List<string>>();
@@ -27,7 +29,7 @@ public class DataBaseScreenFunctions: MonoBehaviour
         // table
         List<List<string>> mock = new List<List<string>>();
         int count = 0;
-        for(int i = 0; i< 10; i++)
+        for(int i = 0; i< 17; i++)
         {
             List<string> temp = new List<string>();
             for(int j = 0; j < 4; j++)
@@ -42,7 +44,25 @@ public class DataBaseScreenFunctions: MonoBehaviour
 
     void startTable()
     {
-        //table.table.HeaderColor = Color.cyan;
+        //setup table
+
+        table.Columns = headerMock.Count;
+        table.Rows = tableMock.Count+1;
+
+        //initialize table
+        for(int i=0; i<headerMock.Count; i++)
+        {
+            table.GetCell(0, i).text = headerMock[i];
+        }
+        for(int i=0; i< tableMock.Count; i++)
+        {
+            print("count: " +tableMock[i].Count);
+            for (int j=0; j<tableMock[i].Count; j++)
+            {
+                print("i: " + i + " | j: " + j);
+                table.GetCell(i + 1, j).text = tableMock[i][j]; 
+            }
+        }
     }
 
     // Update is called once per frame
