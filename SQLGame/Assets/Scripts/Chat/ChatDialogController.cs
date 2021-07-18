69ca0207e097e841598898987716049ac3060ffc
joycebrum <joycebrumu.u@gmail.com>
2021-07-18 18:34:00 -0300

    fix: remove all old messages

--- SQLGame/Assets/Scripts/Chat/ChatDialogController.cs
+++ SQLGame/Assets/Scripts/Chat/ChatDialogController.cs
@@ -37,10 +37,25 @@ public class ChatDialogController : MonoBehaviour
      
     public void DestroyAllMessages()
     {
-        foreach( Transform child in messageParentPanel)
+        int i = 0;
+
+        //Array to hold all child obj
+        GameObject[] allChildren = new GameObject[messageParentPanel.childCount];
+
+        //Find all child obj and store to that array
+        foreach (Transform child in messageParentPanel)
+        {
+            allChildren[i] = child.gameObject;
+            i += 1;
+        }
+
+        //Now destroy them
+        foreach (GameObject child in allChildren)
         {
             DestroyImmediate(child.gameObject);
         }
+
+        Debug.Log(messageParentPanel.childCount);
     }
 
     public void ShowMessage()
