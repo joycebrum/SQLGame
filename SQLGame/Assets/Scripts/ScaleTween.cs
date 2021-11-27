b7a8a4c7f5f126a4e7ae82c2d43faa8bbc9b1e68
joycebrum <joycebrumu.u@gmail.com>
2021-11-27 15:27:38 -0300

    feat: animated focus enabled

--- SQLGame/Assets/Scripts/ScaleTween.cs
+++ SQLGame/Assets/Scripts/ScaleTween.cs
@@ -8,14 +8,27 @@ public class ScaleTween : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private Vector3 scaleChange = new Vector3(0.08f, 0.08f, 0.08f);
     [SerializeField] private float duration = 0.2f;
+    [SerializeField] private Vector3 focusScaleChange = new Vector3(0.16f, 0.16f, 0.16f);
+
+    private LTDescr myTween;
 
     public void OnPointerEnter(BaseEventData baseEventData)
     {
-        LeanTween.scale(gameObject, transform.localScale += scaleChange, duration);
+        LeanTween.scale(gameObject, transform.localScale + scaleChange, duration);
     }
 
     public void OnPointerExit(BaseEventData baseEventData)
     {
-        LeanTween.scale(gameObject, transform.localScale -= scaleChange, duration);
+        LeanTween.scale(gameObject, transform.localScale - scaleChange, duration);
+    }
+
+    public void FocusWithAnimation()
+    {
+        myTween = LeanTween.scale(gameObject, transform.localScale + scaleChange, 0.5f).setLoopType(LeanTweenType.pingPong);
+    }
+
+    public void UnfocusWithAnimation()
+    {
+        LeanTween.cancel(myTween.id);
     }
 }
