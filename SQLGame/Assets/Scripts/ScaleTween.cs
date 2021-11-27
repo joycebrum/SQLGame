1af27c6ff5e9af92bc920712c318c55ff0fe56bd
joycebrum <joycebrumu.u@gmail.com>
2021-11-27 14:41:21 -0300

    feat: create function to interactive button

---
+++ SQLGame/Assets/Scripts/ScaleTween.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ScaleTween : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Vector3 scaleChange = new Vector3(0.08f, 0.08f, 0.08f);
+    [SerializeField] private float duration = 0.2f;
+
+    public void OnPointerEnter(BaseEventData baseEventData)
+    {
+        LeanTween.scale(gameObject, transform.localScale += scaleChange, duration);
+    }
+
+    public void OnPointerExit(BaseEventData baseEventData)
+    {
+        LeanTween.scale(gameObject, transform.localScale -= scaleChange, duration);
+    }
+}
