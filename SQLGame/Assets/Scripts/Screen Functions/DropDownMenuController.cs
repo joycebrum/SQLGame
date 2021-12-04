25286446440153c7ed0db91aa1e53cf424c276bb
joycebrum <joycebrumu.u@gmail.com>
2021-12-04 14:19:57 -0300

    feat: control drop down menu by controller

---
+++ SQLGame/Assets/Scripts/Screen Functions/DropDownMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDownMenuController : MonoBehaviour
+{
+    private bool open = false;
+    private float hiddenDuration = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HiddenWithScale();
+    }
+
+    public void Toggle()
+    {
+        if(open)
+        {
+            HiddenWithScale();
+        }
+        else
+        {
+            ShowWithScale();
+        }
+        open = !open;
+    }
+
+    private void HiddenWithScale()
+    {
+        LeanTween.scale(gameObject.GetComponent<RectTransform>(), new Vector3(1, 0, 1), hiddenDuration);
+    }
+
+    private void ShowWithScale()
+    {
+        LeanTween.scale(gameObject.GetComponent<RectTransform>(), new Vector3(1, 1, 1), hiddenDuration);
+    }
+}
