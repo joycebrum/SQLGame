7963b06e1576c26b0331e16e6aaa47dffef97497
joycebrum <joycebrumu.u@gmail.com>
2021-12-11 18:05:07 -0300

    fix: stage1dbcontroller to create the correct database

--- SQLGame/Assets/Scripts/Stages/StageOneDBController.cs
+++ SQLGame/Assets/Scripts/Stages/StageOneDBController.cs
@@ -1,37 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class StageOneDBController : MonoBehaviour
 {
     public DataBase database;
+    private string sqlCreatePath = "Assets/Resources/Stage 1/createDB.txt";
+    private string sqlPopulatePath = "Assets/Resources/Stage 1/populateDB.txt";
+    private string dbPath = "db/Stage1SQLite.db";
     // Start is called before the first frame update
     void Start()
     {
-        StartDB();
-        PopulateDB();
+        InitDabaBase();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitDabaBase()
     {
-        
+        if (!System.IO.File.Exists(dbPath))
+        {
+            CreateDataBase();
+            PopulateDataBase();
+        }
     }
 
-    void StartDB()
+    private void CreateDataBase()
     {
-        this.database.Connect("URI=file:db/MasterSQLite.db");
-        string sql = "drop table teste";
-        database.QueryCommand(sql);
-        sql = "CREATE TABLE teste(name VARCHAR(20), score INT)";
+        this.database.Connect("URI=file:" + this.dbPath);
+
+        database.NonQueryCommand(ReadFromFile(sqlCreatePath));
+    }
+
+    private void PopulateDataBase()
+    {
+        string sql = ReadFromFile(sqlPopulatePath);
         database.NonQueryCommand(sql);
     }
 
-    void PopulateDB()
+    private string ReadFromFile(string path)
     {
-        string sql = "INSERT INTO teste(name, score) VALUES('Thiago', 45)";
-        database.NonQueryCommand(sql);
-        sql = "INSERT INTO teste(name, score) VALUES('Jocye', 40)";
-        database.NonQueryCommand(sql);
+        StreamReader reader = new StreamReader(path);
+        string sqlCreateText = reader.ReadToEnd();
+        reader.Close();
+        return sqlCreateText;
     }
 }
