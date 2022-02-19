c7796ae5bf6b2b6ddd720c2be99e5b7f236b7783
joycebrum <joycebrumu.u@gmail.com>
2022-02-19 14:24:37 -0300

    feat: save name and full name of player

--- SQLGame/Assets/Scripts/Main/OperationalSystemController.cs
+++ SQLGame/Assets/Scripts/Main/OperationalSystemController.cs
@@ -16,7 +16,8 @@ public class OperationalSystemController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //PlayerPrefs.SetString("playerName", "Joyce");
+        PlayerPrefs.SetString("playerName", "Cris");
+        PlayerPrefs.SetString("playerFullName", "Cristiano Pereira");
     }
 
     public void OnHomeClick()
