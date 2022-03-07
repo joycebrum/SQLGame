41db251d169a692c5aebb7a7962a091fd027614b
Thiago <topd97@yahoo.com.br>
2022-03-07 11:43:06 -0300

    Making buttonns tutorial

--- SQLGame/Assets/Scripts/Tutorial/TutorialController.cs
+++ SQLGame/Assets/Scripts/Tutorial/TutorialController.cs
@@ -59,19 +59,27 @@ public class TutorialController : MonoBehaviour
         List<Transform> components = new List<Transform>(parentPanel.GetComponentsInChildren<Transform>());
         int actualIndex = 0; 
         string highlightName = tutorialStep.gameObject.name;
+        print("-----------");
         foreach (Transform panel in components)
         {
             if (panel.parent.name == "Tutorial" || panel.parent.name == "Tutorial panel" || panel.parent.name == "Menu Drop Down" || panel.name == "Buttons Panel")
             {
                 continue;
-            } else if (panel.name == "Tutorial panel") 
+            } else if (panel.name == "Tutorial panel")
             {
+                print(panel.name + " - 2");
+                panel.SetSiblingIndex(2);
+            } else if (panel.name == "Menu Drop Down" && tutorialStep.gameObject.transform.parent.gameObject.name == "Menu Drop Down")
+            {
+                print(panel.name + " - 3");
                 panel.SetSiblingIndex(3);
             } else if (panel.name == highlightName)
             {
-                panel.SetSiblingIndex(4);
+                print(panel.name + " - 3");
+                panel.SetSiblingIndex(3);
             } else
             {
+                print(panel.name + " - " + actualIndex);
                 panel.SetSiblingIndex(actualIndex);
                 actualIndex++;
             }
@@ -83,26 +91,20 @@ public class TutorialController : MonoBehaviour
 
     private void NextInstruction(TutorialStep tutorialStep)
     {
-        print("começei");
         if(instructionIdx == 0)
         {
-            print("antes if dentro");
             if (tutorialStepIdx > 0) tutorialSteps[tutorialStepIdx-1].gameObject.GetComponent<ButtonAnimationController>().UnfocusWithAnimation();
-            print("depois if dentro");
             tutorialStep.gameObject.GetComponent<ButtonAnimationController>().FocusWithAnimation();
         }
-        print("pasou if 1");
 
         instructionText.text = tutorialStep.instructions[instructionIdx];
         instructionIdx += 1;
 
-        print("antes if 2");
         if (instructionIdx >= tutorialStep.instructions.Length)
         {
             instructionIdx = 0;
             tutorialStepIdx += 1;
         }
-        print("pasou if 2");
     }
 
 
