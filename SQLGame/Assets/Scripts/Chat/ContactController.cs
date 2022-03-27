48d5338172ff2573da496f769c2b907eb21ba4b3
joycebrum <joycebrumu.u@gmail.com>
2022-03-27 19:47:52 -0300

    feat: add notification to contact and message

--- SQLGame/Assets/Scripts/Chat/ContactController.cs
+++ SQLGame/Assets/Scripts/Chat/ContactController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class ContactController : MonoBehaviour
 {
     [SerializeField] private ChatEnum type;
+    [SerializeField] private GameObject notification;
 
     // Start is called before the first frame update
     void Start()
     {
+        ShowNotification();
         if(PlayerPrefs.GetInt("ShouldShow" + GetDialogName()) == 1) {
             this.gameObject.SetActive(true);
         }
@@ -22,10 +24,30 @@ public class ContactController : MonoBehaviour
     {
         return ChatDialogController.GetDialogueName(type);
     }
+
+    public bool HasNotification()
+    {
+        int dialogStatus = PlayerPrefs.GetInt(GetDialogName());
+        return dialogStatus == 2 || dialogStatus == 0;
+    }
+
+    public void ShowNotification()
+    {
+        if(HasNotification())
+        {
+            this.notification.SetActive(true);
+        }
+        else
+        {
+            this.notification.SetActive(false);
+        }
+    }
+
     public void ShowContact()
     {
         PlayerPrefs.SetInt("ShouldShow" + GetDialogName(), 1);
         this.gameObject.SetActive(true);
+        ShowNotification();
     }
 
 }
