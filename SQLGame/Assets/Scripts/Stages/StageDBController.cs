75ffe0c1112037432a54b949a8346bcacc941e1c
joycebrum <joycebrumu.u@gmail.com>
2022-04-10 11:59:53 -0300

    fix: db generation on simulation

--- SQLGame/Assets/Scripts/Stages/StageDBController.cs
+++ SQLGame/Assets/Scripts/Stages/StageDBController.cs
@@ -8,19 +8,11 @@ public class StageDBController : MonoBehaviour
     public DataBase database;
     public void InitDabaBase(string dbPath, string sqlCreatePath, string sqlPopulatePath)
     {
-        print("inicio");
         bool fileExists = System.IO.File.Exists(dbPath);
-        print("file exists: " + fileExists);
         this.database.Connect(dbPath);
-        print("terminou o connect");
-        if (!fileExists)
-        {
-            print("antes do create");
-            CreateDataBase(sqlCreatePath);
-            print("depois do create");
-            PopulateDataBase(sqlPopulatePath);
-            print("depois do populate");
-        }
+        
+        CreateDataBase(sqlCreatePath);
+        PopulateDataBase(sqlPopulatePath);
     }
 
     private void CreateDataBase(string sqlCreatePath)
