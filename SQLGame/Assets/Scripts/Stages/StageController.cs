71e42440c914ea5a8a71cc533696ec1614455234
joycebrum <joycebrumu.u@gmail.com>
2022-07-02 17:05:12 -0300

    feat: finish game if does not have other stage

--- SQLGame/Assets/Scripts/Stages/StageController.cs
+++ SQLGame/Assets/Scripts/Stages/StageController.cs
@@ -72,6 +72,10 @@ public class StageController : MonoBehaviour
             main.SetupStage(currentStageIndex: currentStageIndex);
             StartStage();
         }
+        else
+        {
+            currentStage.FinishGame();
+        }
     }
 
     public void StartStage()
